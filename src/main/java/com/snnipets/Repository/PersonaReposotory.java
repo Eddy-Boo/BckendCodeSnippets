a6f424e869af4027533131e0a206d424cd311dd0
Bryam11 <xavierchuchuca18@gmail.com>
2020-09-16 17:12:49 -0500

    Create the class interface of person

---
+++ src/main/java/com/snnipets/Repository/PersonaReposotory.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.snnipets.Repository;
+
+import com.snnipets.Model.Persona;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ *
+ * @author brhyant
+ */
+public interface PersonaReposotory extends MongoRepository< Persona, String>{
+    
+}
