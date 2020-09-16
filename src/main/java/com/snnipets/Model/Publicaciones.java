f0dba90ff8ba6a85073a5804d3bba332f0a40e5a
Eddy-Boo <68482691+Eddy-Boo@users.noreply.github.com>
2020-09-16 17:43:04 -0500

    Build alls the class and properties

---
+++ src/main/java/com/snnipets/Model/Publicaciones.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.snnipets.Model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author edy
+ */
+@Data
+@Document
+public class Publicaciones {
+    private String codigo;
+    private String Descripcion;
+}
