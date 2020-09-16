03291c4eb55c35b5d412571bcbfcaaf51c741584
Eddy-Boo <68482691+Eddy-Boo@users.noreply.github.com>
2020-09-16 16:43:39 -0500

    Master

---
+++ src/test/java/com/snnipets/CodeSnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snnipets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeSnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
