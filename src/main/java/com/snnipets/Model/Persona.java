e3509fc36a5dac8219030ab030e30c0602f5db05
Bryam11 <xavierchuchuca18@gmail.com>
2020-09-16 17:09:35 -0500

    Comit of packages and class Person

---
+++ src/main/java/com/snnipets/Model/Persona.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.snnipets.Model;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ *
+ * @author brhyant
+ */
+@Data
+@Document
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String edad;
+    private String email;
+}
