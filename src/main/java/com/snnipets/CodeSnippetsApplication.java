9cb75c6b0c2ce2113ff56af15ac628a9b018c7de
Bryam11 <xavierchuchuca18@gmail.com>
2020-09-17 19:44:39 -0500

    spacion in clas main

--- src/main/java/com/snnipets/CodeSnippetsApplication.java
+++ src/main/java/com/snnipets/CodeSnippetsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CodeSnippetsApplication {
+    
+   
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeSnippetsApplication.class, args);
